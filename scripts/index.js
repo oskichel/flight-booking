8298eb40fcfa26da4b0e655177362b955f0fdca0
Elvina Kurseitova <el.oskich@gmail.com>
2022-02-24 00:21:29 +0300

    Приложение для бронирования авиабилетов

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+import start from "./modules/start.js";
+import getFormPerson from "./modules/formPerson.js";
+import readyPlane from "./modules/readyPlane.js";
+import getData from "./service/getTour.js";
+
+const init = async (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
+    const data = await getData();
+
+    const {main, firstForm, h1} = start(app, title, data)
+
+    firstForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const tourData = data.find(tour => tour.id === firstForm.tour.value);
+        h1.textContent = tourData.tour;
+
+        const forms = getFormPerson(firstForm.count.value);
+
+        firstForm.remove();
+
+        main.append(...forms)
+
+        readyPlane(forms, main, tourData);
+    })
+};
+
+init('.app', 'Выберите тур');
---
+++ scripts/modules/airplane.js
@@ -0,0 +1,123 @@
+import createElement from "./createElement.js";
+import declOfNum from "./declOfNum.js";
+
+const createCockpit = (titleText) => {
+    const cockpit = createElement('div', {
+        className: 'cockpit',
+    });
+
+    const title = createElement('h1', {
+        className: 'cockpit-title',
+        textContent: titleText,
+    });
+
+    const button = createElement('button', {
+        className: 'cockpit-confirm',
+        type: 'submit',
+        textContent: 'Подтвердить',
+    });
+
+    cockpit.append(title, button);
+
+    return cockpit;
+};
+
+const createExit = () => {
+    const fuselage = createElement('div', {
+        className: 'fuselage exit',
+    });
+
+    return fuselage;
+};
+
+const createBlockSeat = (n, count) => {
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+    const fuselage = createElement('ol', {
+        className: 'fuselage',
+    });
+
+    for (let i = n; i < count + n; i++) {
+        const wrapperRow = createElement('li');
+        const seats = createElement('ol', {
+            className: 'seats',
+        });
+
+        const seatsRow = letters.map(letter => {
+            const seat = createElement('li', {
+                className: 'seat',
+            });
+            const wrapperCheck = createElement('label');
+            const check = createElement('input', {
+                className: 'seat',
+                type: 'checkbox',
+                value: `${i}${letter}`,
+            });
+
+            wrapperCheck.append(check);
+            seat.append(wrapperCheck);
+            return seat;
+        })
+
+        seats.append(...seatsRow);
+        wrapperRow.append(seats);
+        fuselage.append(wrapperRow);
+    }
+
+    return fuselage;
+};
+
+const createAirplane = (title, tourData) => {
+    const scheme = tourData.scheme;
+    const choisesSeat = createElement('form', {
+        className: 'choises-seat',
+    });
+
+    const plane = createElement('fieldset', {
+        className: 'plane',
+        name: 'plane',
+    });
+
+    const cockpit = createCockpit(title);
+
+    let n = 1
+
+    const elements = scheme.map((type) => {
+        if (type === 'exit') {
+            return createExit();
+        }
+        if (typeof type === 'number') {
+            const blockSeat = createBlockSeat(n, type);
+            n = n + type;
+            return blockSeat;
+        }
+    });
+
+    plane.append(cockpit, ...elements);
+
+    choisesSeat.append(plane);
+
+    return choisesSeat;
+};
+
+const checkSeat = (form, data) => {
+    form.addEventListener('change', () => {
+        console.log('change');
+        const formData = new FormData(form);
+        const checked = [...formData].map(item => {
+            console.log(item);
+        })
+    })
+};
+
+const airplane = (main, data, tourData) => {
+    const title = `Выберите ${declOfNum(data.length, ['место', 'места', 'мест'])}`;
+
+    const choiseForm = createAirplane(title, tourData);
+
+    checkSeat(choiseForm, data);
+
+    main.append(choiseForm);
+};
+
+export default airplane;
